afd6e5f80a76d48c74a929151c30b5bf54c111d5
isicher <tech2dom@gmail.com>
2025-11-23 08:56:13 +0100

    still working

--- src/modules/Quotes/Quotes.service.ts
+++ src/modules/Quotes/Quotes.service.ts
@@ -6,6 +6,12 @@ export class QuotesService {
   constructor(private prisma: PrismaService) {}
   async getQuoteOfTheDay() {
     const count = await this.prisma.quotes.count();
+    if (count === 0) {
+      return {
+        text: 'The journey of a thousand miles begins with a single step.',
+        author: 'Lao Tzu',
+      };
+    }
     const randomIndex = Math.floor(Math.random() * count);
     const quote = await this.prisma.quotes.findMany({
       take: 1,
